17c1881b1bb4cd064fd6819c0a5f5dcd79ff5d43
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-09 21:56:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(string)
-  array = string.split(" ")
-  new_array = []
-  array.each do|reverse|
-    new_array << reverse.reverse
-  end
-  new_array.join(" ")
-end
+#def reverse_each_word(string)
+ # array = string.split(" ")
+  #new_array = []
+  #array.each do|reverse|
+   # new_array << reverse.reverse
+  #end
+  #new_array.join(" ")
+#end
 
 def reverse_each_word(string)
   array = string.split(" ")
